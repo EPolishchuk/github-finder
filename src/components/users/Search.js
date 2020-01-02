ce999c4ddbf1a94296506ff6bf27d4a6d40d15b8
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2020-01-02 22:29:45 +0200

    Added search functionality

---
+++ src/components/users/Search.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Search extends Component {
+  state = {
+    text: ''
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  onChange = e => this.setState({ text: e.target.value });
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          ></input>
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          ></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
